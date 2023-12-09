de4976bfa655286e680e47634c452b0056e1e398
SunBK201 <sunbk201gm@gmail.com>
2023-12-09 16:30:36 +0800

    test

--- api/message.ts
+++ api/message.ts
@@ -2,7 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
     var data = {
-        msg: "Hello World" + req.headers['user-agent']?.toString(),
+        ua: req.headers['user-agent']?.toString(),
     };
 
     res.status(200).json(data);
