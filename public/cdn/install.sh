dd2c3039f8bff6e7f09cd1c625afeaa09330e6c6
SunBK201 <sunbk201gm@gmail.com>
2024-02-17 23:19:52 +0800

    update cdn

--- public/cdn/install.sh
+++ public/cdn/install.sh
@@ -92,6 +92,5 @@ rm /tmp/luci-modulecache/* >/dev/null 2>&1
 rm /tmp/luci-indexcache* >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    service ua3f.service restart
     echo "Install UA3F Success."
 fi
